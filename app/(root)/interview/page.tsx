72d7adf29c483bf61fa3927422a16008a6a3ae8e
johnson williams <williams.gb55@gmail.com>
2025-03-31 16:03:44 +0900

    vapi-agent-setup

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,13 +1,16 @@
 import Agent from "@/components/Agent";
+import { getCurrentUser } from "@/lib/actions/auth.action";
 import React from "react";
 
-const page = () => {
+const Page = async () => {
+  const user = await getCurrentUser();
+
   return (
     <>
       <h3>Interview Generation</h3>
-      <Agent userName="You" userId="user1" type="generate" />
+      <Agent userName={user?.name} userId={user?.id} type="generate" />{" "}
     </>
   );
 };
 
-export default page;
+export default Page;

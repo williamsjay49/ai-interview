8d8becd254d3ef2c18d1fb49bbb0596bba4411de
johnson williams <williams.gb55@gmail.com>
2025-03-31 20:41:17 +0900

    final-commit

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,14 +1,19 @@
 import Agent from "@/components/Agent";
 import { getCurrentUser } from "@/lib/actions/auth.action";
-import React from "react";
 
 const Page = async () => {
   const user = await getCurrentUser();
 
   return (
     <>
-      <h3>Interview Generation</h3>
-      <Agent userName={user?.name} userId={user?.id} type="generate" />{" "}
+      <h3>Interview generation</h3>
+
+      <Agent
+        userName={user?.name!}
+        userId={user?.id}
+        profileImage={user?.profileURL}
+        type="generate"
+      />
     </>
   );
 };
--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -1,11 +1,14 @@
 import dayjs from "dayjs";
-import Image from "next/image";
-import { getRandomInterviewCover } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import Image from "next/image";
+
+import { Button } from "./ui/button";
 import DisplayTechIcons from "./DisplayTechIcons";
 
-const InterviewCard = ({
+import { cn, getRandomInterviewCover } from "@/lib/utils";
+import { getFeedbackByInterviewId } from "@/lib/actions/general.action";
+
+const InterviewCard = async ({
   id,
   userId,
   role,
@@ -13,8 +16,23 @@ const InterviewCard = ({
   techstack,
   createdAt,
 }: InterviewCardProps) => {
-  const feedback = null as Feedback | null;
+  const feedback =
+    userId && id
+      ? await getFeedbackByInterviewId({
+          interviewId: id,
+          userId,
+        })
+      : null;
+
   const normalizedType = /mix/gi.test(type) ? "Mixed" : type;
+
+  const badgeColor =
+    {
+      Behavioral: "bg-light-400",
+      Mixed: "bg-light-600",
+      Technical: "bg-light-800",
+    }[normalizedType] || "bg-light-600";
+
   const formattedDate = dayjs(
     feedback?.createdAt || createdAt || Date.now()
   ).format("MMM D, YYYY");
@@ -23,40 +41,50 @@ const InterviewCard = ({
     <div className="card-border w-[360px] max-sm:w-full min-h-96">
       <div className="card-interview">
         <div>
-          <div className="absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg bg-light-600">
-            <p className="badge-text">{normalizedType}</p>
+          {/* Type Badge */}
+          <div
+            className={cn(
+              "absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg",
+              badgeColor
+            )}
+          >
+            <p className="badge-text ">{normalizedType}</p>
           </div>
 
+          {/* Cover Image */}
           <Image
             src={getRandomInterviewCover()}
-            alt="cover image"
+            alt="cover-image"
             width={90}
             height={90}
             className="rounded-full object-fit size-[90px]"
           />
 
+          {/* Interview Role */}
           <h3 className="mt-5 capitalize">{role} Interview</h3>
 
+          {/* Date & Score */}
           <div className="flex flex-row gap-5 mt-3">
             <div className="flex flex-row gap-2">
               <Image
                 src="/calendar.svg"
-                alt="calendar"
                 width={22}
                 height={22}
+                alt="calendar"
               />
               <p>{formattedDate}</p>
             </div>
 
             <div className="flex flex-row gap-2 items-center">
-              <Image src="/star.svg" alt="star" width={22} height={22} />
+              <Image src="/star.svg" width={22} height={22} alt="star" />
               <p>{feedback?.totalScore || "---"}/100</p>
             </div>
           </div>
 
+          {/* Feedback or Placeholder Text */}
           <p className="line-clamp-2 mt-5">
             {feedback?.finalAssessment ||
-              "You haven't taken the interview yet. Take it now to improve your skills."}
+              "You haven't taken this interview yet. Take it now to improve your skills."}
           </p>
         </div>
 
@@ -65,7 +93,11 @@ const InterviewCard = ({
 
           <Button className="btn-primary">
             <Link
-              href={feedback ? `/interview/${id}/feedback` : `/interview/${id}`}
+              href={
+                feedback
+                  ? `/interview/${interviewId}/feedback`
+                  : `/interview/${interviewId}`
+              }
             >
               {feedback ? "Check Feedback" : "View Interview"}
             </Link>
@@ -75,4 +107,5 @@ const InterviewCard = ({
     </div>
   );
 };
+
 export default InterviewCard;
